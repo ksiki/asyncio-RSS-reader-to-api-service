651aefa44f6060adb19358b175bda8f0db8bffdb
ksiki <j.kolyasha@gmail.com>
2026-04-28 14:14:59 +0300

    open/closer app loop

--- api_service/src/main.py
+++ api_service/src/main.py
@@ -5,7 +5,9 @@ from fastapi import FastAPI
 
 from api_service.src.api.v1.api import api_router as api_v1
 from api_service.src.core.config import settings
+from common.database.core.redis_service import redis_service
 from common.tkq import broker
+from rss_reader.core.fetcher import fetcher
 
 
 @asynccontextmanager
@@ -13,6 +15,8 @@ async def lifespan(app: FastAPI):
     await broker.startup()
     yield
     await broker.shutdown()
+    await redis_service.close()
+    await fetcher.close()
 
 
 app: Final[FastAPI] = FastAPI(lifespan=lifespan)
